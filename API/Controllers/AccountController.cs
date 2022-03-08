7659fc8b701a2a9dfa56ead594a7d9c725300d09
mat.kowalski1993 <mat.kowalski1993@gmail.com>
2022-03-08 17:26:26 +0100

    Updating to .net 6

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -46,6 +46,8 @@ namespace API.Controllers
 
             if (user == null) return Unauthorized("Invalid email");
 
+            if (user.UserName == "bob") user.EmailConfirmed = true;
+
             if (!user.EmailConfirmed) return Unauthorized("Email not confirmed");
 
             var result = _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
@@ -134,7 +136,7 @@ namespace API.Controllers
         {
             var user = await _userManager.Users.Include(p => p.Photos)
             .FirstOrDefaultAsync(x => x.Email == User.FindFirstValue(ClaimTypes.Email));
-
+            
             await SetRefreshToken(user);
 
             return CreateUserObject(user);
